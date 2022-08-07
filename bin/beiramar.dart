ccd9c8acc7e0705d0c4c4c5809653e53a07739b6
Eduardo Folly <edufolly@gmail.com>
2022-08-07 03:28:03 +0200

    Event socket.

--- bin/beiramar.dart
+++ bin/beiramar.dart
@@ -1,5 +1,70 @@
-import 'package:beiramar/beiramar.dart' as beiramar;
+// ignore_for_file: cascade_invocations
 
-void main(List<String> arguments) {
-  print('Hello world: ${beiramar.calculate()}!');
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+// https://github.com/dart-lang/dart-docker/
+///
+///
+///
+void main(List<String> arguments) async {
+  print('\nStarting...\n\n');
+  InternetAddress host =
+      InternetAddress('/var/run/docker.sock', type: InternetAddressType.unix);
+
+  Socket socket = await Socket.connect(host, 0);
+
+  ProcessSignal.sigint.watch().listen((dynamic event) => socket.close());
+  bool first = true;
+  socket.listen(
+    (Uint8List event) {
+      if (first) {
+        first = false;
+        // TODO: Check if connection is successful.
+        print('First response:');
+        print(String.fromCharCodes(event).trim());
+        print('');
+        return;
+      }
+
+      print('------');
+      if (event.length > 5) {
+        print(event.sublist(0, 5));
+        String body = String.fromCharCodes(event.sublist(5)).trim();
+        print(body);
+        print('Length: ${body.length}');
+
+        Map<String, dynamic> map = json.decode(body);
+        print(map);
+      } else {
+        print(event);
+      }
+      print('||||||');
+    },
+    onError: (dynamic e, StackTrace s) {
+      print('onError: $e');
+      print(s);
+    },
+    onDone: () async {
+      print('onDone');
+      socket.destroy();
+      // await socket.done;
+      exit(0);
+    },
+  );
+
+  socket.writeln('GET /v1.41/events HTTP/1.1');
+  socket.writeln('Host: localhost');
+  socket.writeln('User-Agent: curl/7.81.0');
+  socket.writeln('Accept: */*');
+  socket.writeln();
+
+  // dynamic d = await socket.close();
+
+  // print(d);
+
+  // await socket.done;
+
+  // exit(0);
 }
